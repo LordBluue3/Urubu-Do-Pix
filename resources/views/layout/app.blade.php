bd1cd478ad3e8a4613d663d90b80c304fdc3c666
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-07 14:10:54 -0700

    feat: add logo image

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,7 @@
 <body>
     <header>
         <nav>
+            <img src="https://cdn.discordapp.com/attachments/701489669673844887/1160261039120724069/pix_icon_198027.png?ex=6534044c&is=65218f4c&hm=0e7fb393edc5bbca3c66b51454037fc99c1f769b30d77e3489505af5d1aa62a5&" alt="Logo do pix">
             <ul>
                 <li><a href="{{route('home')}}">Inicio</a></li>
                 <li><a href="{{route('login')}}">Login</a></li>
