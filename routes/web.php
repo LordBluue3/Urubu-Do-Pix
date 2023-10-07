cff3a28fcb1d754b8fced6f77e7c3f4dd09348db
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-07 13:35:30 -0700

    feat: create routes for login, home and register pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
