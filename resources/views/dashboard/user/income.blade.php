d325d1af4d6bbd8a3430019f2a77ce5ceda533d2
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 10:44:19 -0700

    feat: add modal in the income page
    - get the fake money in the database

--- resources/views/dashboard/user/income.blade.php
+++ resources/views/dashboard/user/income.blade.php
@@ -1,13 +1,41 @@
 @extends('layout/user-dashboard')
 
 @push('head')
+    @vite(['resources/scss/register.scss'])
     @vite(['resources/scss/dashboard/user/income.scss'])
 @endpush
 
 @section('content')
     <div class="container_income">
+        <!-- Modal Depositar -->
+        <div id="myModal1" class="modal">
+            <div class="modal_content">
+                <span class="close" data-modal="#myModal1">&times;</span>
+                <h1>Escolha a quantia</h1>
+                <form action="{{ route('dashboard.user.deposit.update') }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <input name="money" type="number" placeholder="Valor" required>
+                    <div class="container_button">
+                        <button>Depositar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <!-- Modal Sacar -->
+        <div id="myModal2" class="modal">
+            <div class="modal_content">
+                <span class="close" data-modal="#myModal2">&times;</span>
+                <img style="width: 50%"
+                    src="https://cdn.discordapp.com/attachments/701489669673844887/1160569448399376399/47106.png?ex=65352386&is=6522ae86&hm=8d4f15c4d4586ec881968889d3e3a9383152e3a20d6b996233b43f3e12f99821&"
+                    alt="Urubu">
+                <h1>Você caiu em um golpe!!!!</h1>
+            </div>
+        </div>
+
         <div class="container_card_results">
-            <p class="money">R$ 1000</p>
+            <p class="money">R$ {{ $fakeMoney }}</p>
 
             <div class="container_yield">
                 <div class="triangle"></div>
@@ -15,8 +43,45 @@
             </div>
         </div>
         <div class="container_buttons">
-            <button>Depositar</button>
-            <button id="withdraw">Sacar</button>
+            <button class="modal_btn" data-modal="#myModal1">Depositar</button>
+            <button id="withdraw" class="modal_btn" data-modal="#myModal2">Sacar</button>
         </div>
     </div>
+    </div>
 @endsection
+
+@push('scripts')
+    <script>
+        function toggleModal(modalId) {
+            var modal = document.querySelector(modalId);
+            if (modal) {
+                modal.style.display = modal.style.display === "block" ? "none" : "block";
+            }
+        }
+
+        var modalBtns = document.querySelectorAll(".modal_btn");
+        modalBtns.forEach(function(btn) {
+            btn.addEventListener("click", function() {
+                var modalId = this.getAttribute("data-modal");
+                toggleModal(modalId);
+            });
+        });
+
+        var closeBtns = document.querySelectorAll(".close");
+        closeBtns.forEach(function(btn) {
+            btn.addEventListener("click", function() {
+                var modalId = this.getAttribute("data-modal");
+                toggleModal(modalId);
+            });
+        });
+
+        window.addEventListener("click", function(event) {
+            var modals = document.querySelectorAll(".modal");
+            modals.forEach(function(modal) {
+                if (event.target === modal) {
+                    modal.style.display = "none";
+                }
+            });
+        });
+    </script>
+@endpush
