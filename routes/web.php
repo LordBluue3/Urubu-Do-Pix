bebca3e9a30e8b36f2c375c868cf7679be1eaf01
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 07:27:39 -0700

    feat: create private routes for the dashboards

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\InformationController;
+use App\Http\Controllers\ListUserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostNewController;
 use App\Http\Controllers\RegisterController;
 
 
@@ -26,8 +28,17 @@ Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-Route::get('/dashboard/user', [IncomeController::class, 'show'])->name('dashboard.user');
 
-Route::get('/dashboard/user/information', [InformationController::class, 'show'])->name('dashboard.user.information');
+Route::prefix('/dashboard/user')->middleware(['auth:web'])->group(function(){
+    Route::get('/', [IncomeController::class, 'show'])->name('dashboard.user');
+    Route::get('/information', [InformationController::class, 'show'])->name('dashboard.user.information');
+    
+    
+});
+Route::prefix('/dashboard/admin')->middleware(['auth:admins'])->group(function(){
+    Route::get('/', [ListUserController::class, 'show'])->name('dashboard.admin');
+    Route::get('/posts', [PostNewController::class, 'show'])->name('dashboard.admin.posts');
+    
+});
 
 
