6f5dc01df974e63abcc5a1ed2282a9844b030171
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 00:40:17 -0700

    feat: create information controller

---
+++ app/Http/Controllers/InformationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InformationController extends Controller
+{
+    public function show(){
+    return view('dashboard.user.information');
+    }
+}
