f38ec1264e21dadd3f4e16da5be0b35bd1513c77
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-07 17:18:21 -0700

    feat: add csrf

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
 <form action="{{route('login.store')}}" method="POST">
+    @csrf
     <input name="email" type="email" placeholder="Email" required>
     <input name="password" type="password" placeholder="Senha" required>
     <div class="container_button">
