3b99cc1440fbb886628c94ed66d8f36c81f9852a
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 10:53:37 -0700

    feat: return the informations about the user
    - takes the user authenticated and return the information for the view

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class InformationController extends Controller
 {
     public function show()
     {
-        return view('dashboard.user.information');
+
+        $user = Auth::user();
+
+        $name = $user->name;
+        $phone = $user->phone;
+        $email = $user->email;
+
+        return view('dashboard.user.information', compact('name', 'phone', 'email'));
+
+
     }
 }
