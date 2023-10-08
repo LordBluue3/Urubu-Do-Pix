61030be0a6f36e84c022dbc2a0e22c06f5ac5bee
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 10:51:51 -0700

    feat: return the fake money for user

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IncomeController extends Controller
 {
     public function show(){
-        return view('dashboard.user.income');
+        $user = Auth::user();
+        $fakeMoney = $user->fake_money;
+        return view('dashboard.user.income', compact('fakeMoney'));
     }
 }
