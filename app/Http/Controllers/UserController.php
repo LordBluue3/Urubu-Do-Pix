a516a304f8a57ed90f40866d292116ec9cbac353
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 10:51:13 -0700

    feat: create update information and update money method
    - update information.
    takes the authenticated user and updates the rows
    
    - update money.
    takes the authenticated user and update the money and fake money

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 class UserController extends Controller
 {
@@ -32,6 +34,36 @@ class UserController extends Controller
         } catch (\Exception $e) {
             return redirect('/register')->with('error', 'User creation failed: ' . $e->getMessage());
         }
+
+    }
+    public function updateInformation(Request $request, User $user)
+    {
+        $name = $request->input('name');
+        $phone = $request->input('phone');
+        $email = $request->input('email');
+
+        $userRegister = Auth::user();
+
+        if ($userRegister) {
+            $userRegister->name = $name;
+            $userRegister->phone = $phone;
+            $userRegister->email = $email;
+            $userRegister->save();
+        }
+        return redirect('/dashboard/user/information');
+    }
+
+    public function updateMoney(Request $request)
+    {
+        $money = $request->input('money');
+        $user = Auth::user();
+        if ($user) {
+            $user->money = $user->money + $money;
+            $user->fake_money = $user->fake_money + $money;
+            $user->save();
+        }
+
+        return redirect('/dashboard/user');
 
     }
 }
