25251a300d392e4c948beda1a74f6b9f3a5d1795
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 16:33:03 -0700

    feat: return the posts for view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function show(){
-        return view('home');
+        $posts = Post::all();
+        return view('home', compact('posts'));
     }
 }
