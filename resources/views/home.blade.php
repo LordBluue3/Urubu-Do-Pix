1da11b8f029da27eb6ac6c399318e273de72f566
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-07 23:46:21 -0700

    feat: edit the structure of the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,19 @@
 @endpush
 
 @section('content')
-    <div class="container_image">
-        <img src="https://cdn.discordapp.com/attachments/701489669673844887/1160249280603377806/noticiasconcursos.png?ex=6533f958&is=65218458&hm=1edd86893250d20b5da498522ea0cf878c60c0704a94867847684f33269e6398&"
-            alt="Imagem de um Urubu ao lado da logo do Pix">
-    </div>
-    <div class="container_posts">
-        <div class="container_title">
-            <h1>Urubu do Pix</h1>
+    <section class="container_home">
+        <div class="container_image">
+            <img src="https://cdn.discordapp.com/attachments/701489669673844887/1160249280603377806/noticiasconcursos.png?ex=6533f958&is=65218458&hm=1edd86893250d20b5da498522ea0cf878c60c0704a94867847684f33269e6398&"
+                alt="Imagem de um Urubu ao lado da logo do Pix">
         </div>
-        <p>rendendo muito mais que os invetimentos atuais do mercado!</p>
-        <p class="date">07/10/2023</p>
-    <div>
+        <div class="container_posts">
+            <h1 class="title_news">Novidades</h1>
+            <div class="container_post">
+                <div class="container_title">
+                    <h1 class="title_post">Urubu do Pix</h1>
+                </div>
+                <p>rendendo muito mais que os invetimentos atuais do mercado!</p>
+                <p class="date">07/10/2023</p>
+            </div>
+    </section>
 @endsection
