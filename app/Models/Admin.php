60c42eb6e59d069bf0f19ae36ecd66a5f073b8e1
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 16:38:14 -0700

    feat: create admin model and implements authenticatable

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Admin extends Model implements Authenticatable
 {
     use HasFactory;
+
+    public function getAuthIdentifierName()
+    {
+        return 'id'; 
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey(); 
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password; 
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value; 
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token'; 
+    }
 }
+
