dff57658909e88c0fc3aa17778110c489243b800
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 00:41:42 -0700

    feat: modify the route in the navbar

--- resources/views/layout/user-dashboard.blade.php
+++ resources/views/layout/user-dashboard.blade.php
@@ -12,8 +12,8 @@
         <nav>
             <img src="https://cdn.discordapp.com/attachments/701489669673844887/1160261039120724069/pix_icon_198027.png?ex=6534044c&is=65218f4c&hm=0e7fb393edc5bbca3c66b51454037fc99c1f769b30d77e3489505af5d1aa62a5&" alt="Logo do pix">
             <ul>
-                <li><a href="{{route('home')}}">Rendimentos</a></li>
-                <li><a href="{{route('login')}}">Informações</a></li>
+                <li><a href="{{route('dashboard.user')}}">Rendimentos</a></li>
+                <li><a href="{{route('dashboard.user.information')}}">Informações</a></li>
                 <li><a href="{{route('register')}}">Sair</a></li>
             </ul>
         </nav>
