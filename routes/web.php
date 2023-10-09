588806941687dc5983825f7dcc8e7434e031963b
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 23:01:56 -0700

    refactor: using the route::view for remove unnecessary controllers

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,9 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\IncomeController;
-use App\Http\Controllers\InformationController;
 use App\Http\Controllers\ListUserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\PostNewController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 /*Start Public Routes* */
 Route::get('/', [HomeController::class, 'show'])->name('home');
 
-Route::get('/login', [LoginController::class, 'show'])->name('login');
+Route::view('/login', 'login')->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
 Route::get('/register', [UserController::class, 'show'])->name('register');
@@ -38,8 +35,8 @@ Route::post('/register', [UserController::class, 'store'])->name('register.store
 
 /*Start User Dashboard* */
 Route::prefix('/dashboard/user')->middleware(['auth:web'])->group(function () {
-    Route::get('/', IncomeController::class)->name('dashboard.user');
-    Route::get('/information', [InformationController::class, 'show'])->name('dashboard.user.information');
+    Route::view('/',  'dashboard.user.income')->name('dashboard.user');
+    Route::view('/information', 'dashboard.user.information')->name('dashboard.user.information');
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout.user');
 
 });
@@ -50,16 +47,16 @@ Route::put('/dashboard/user/deposit', [UserController::class, 'updateMoney'])->n
 /*Start Admin Dashboard* */
 Route::prefix('/dashboard/admin')->middleware(['auth:admins'])->group(function () {
     Route::get('/', [ListUserController::class, 'show'])->name('dashboard.admin');
-    Route::get('/posts', [PostNewController::class, 'show'])->name('dashboard.admin.posts');
+    Route::view('/posts', 'dashboard.admin.post-new')->name('dashboard.admin.posts');
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout.admin');
     Route::post('/post', [PostController::class, 'store'])->name('admin.new.post');
     Route::get('/post', [PostController::class, 'show'])->name('admin.new.post.show');
-    Route::get('/edit/post/{id}', [PostController::class, 'showEdit'])->name('admin.edit.post.show');
+    Route::get('/edit/post/{post}', [PostController::class, 'edit'])->name('admin.edit.post.show');
 
 });
 Route::put('/dashboard/admin/draw', [AdminController::class, 'update'])->name('dashboard.admin.draw.update');
 Route::delete('/dashboard/admin/delete/{id}', [AdminController::class, 'delete'])->name('dashboard.admin.user.delete');
-Route::delete('/dashboard/admin/post/delete/{id}', [PostController::class, 'delete'])->name('dashboard.admin.post.delete');
-Route::put('/dashboard/admin/edit/post/{id}', [PostController::class, 'update'])->name('admin.edit.post.update');
+Route::delete('/dashboard/admin/post/delete/{post}', [PostController::class, 'delete'])->name('dashboard.admin.post.delete');
+Route::put('/dashboard/admin/edit/post/{post}', [PostController::class, 'update'])->name('admin.edit.post.update');
 
 /*End Admin Dashboard* */
