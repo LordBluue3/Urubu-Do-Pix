5f7e3cc486d5748e952d33382a467b34acae04f6
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-09 00:56:38 -0700

    chore: remove session flush

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
     }
     public function logout()
     {
-        Auth::logout()->session()->flush();
+        Auth::logout();
         return redirect()->route('home');
     }
 }
