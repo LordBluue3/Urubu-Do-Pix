eece69893b118cfd0209532892a93fad08668f96
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 23:00:18 -0700

    refactor: improving code readability

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
 
 class PostController extends Controller
 {
@@ -13,46 +12,38 @@ class PostController extends Controller
         $posts = Post::all();
         return view('dashboard.admin.list-posts', compact('posts'));
     }
-    public function showEdit($id)
+    public function edit(Post $post)
     {
-        $post = Post::find($id);
         return view('dashboard.admin.edit-post', compact('post'));
     }
     public function store(Request $request, Post $post)
     {
-        $title = $request->input('title');
-        $description = $request->input('description');
-
-        $dateNow = Carbon::now();
-
-        $post->title = $title;
-        $post->text = $description;
-        $post->date = $dateNow->format('d/m/Y');
-
-        $post->save();
-        return redirect()->back();
+        $post->create([ 
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => now()->format('d/m/Y')
+        ]);
+        return back();
 
     }
-    public function delete($id)
+    public function delete(Post $post)
     {
-        $post = Post::find($id);
         $post->delete();
         return redirect()->back();
     }
     public function update(Request $request, $id)
     {
         $post = Post::find($id);
-        $dateNow = Carbon::now();
 
         $title = $request->input('title');
         $text = $request->input('description');
        
         $post->title = $title;
         $post->text =  $text ;
-        $post->date = $dateNow->format('d/m/Y');
+        $post->date = now()->format('d/m/Y');
         $post->save();
 
-        return redirect('/dashboard/admin/post');
+        return redirect()->route('admin.new.post.show');
     }
 
 }
