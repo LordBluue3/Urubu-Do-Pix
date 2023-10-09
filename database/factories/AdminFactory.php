c60e126ca8c1e93bb0ed92e17e8f87351aa0639d
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 20:55:40 -0700

    feat: create admin factory

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->name,
+            "email" => $this->faker->safeEmail,
+            "password" => bcrypt(123),
+            "money" => 0,
+        ];
+    }
+}
