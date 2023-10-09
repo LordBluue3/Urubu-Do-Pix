f7787840aebdcf79ec0c64ff31bdef4370b54aec
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 23:06:24 -0700

    refactor: remove try catch, add redirect route and add login request

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,36 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function show()
+    public function store(LoginRequest $request)
     {
-        return view('login');
-    }
-    public function store(Request $request)
-    {
-        try {
-            $email = $request->input('email');
-            $password = $request->input('password');
+        $email = $request->input('email');
+        $password = $request->input('password');
 
-            if (Auth::guard('web')->attempt(['email' => $email, 'password' => $password])) {
-                return redirect('/dashboard/user');
-            } else if (Auth::guard('admins')->attempt(['email' => $email, 'password' => $password])) {
-                return redirect('/dashboard/admin');
-            } else {
-                return redirect('/register');
-            }
-        } catch (\Exception $e) {
-            return redirect()->back();
-
+        if (Auth::guard('web')->attempt(['email' => $email, 'password' => $password])) {
+            return redirect()->route('dashboard.user');
+        } else if (Auth::guard('admins')->attempt(['email' => $email, 'password' => $password])) {
+            return redirect()->route('dashboard.admin');
+        } else {
+            return redirect()->route('register');
         }
+
     }
-    public function logout(Request $request)
+    public function logout()
     {
-        Auth::logout();
-        return redirect('/');
+        Auth::logout()->session()->flush();
+        return redirect()->route('home');
     }
 }
