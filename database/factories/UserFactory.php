e3c2fd2c9e96b92e43a5891a1dd3bd44600fb0a3
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2023-10-08 20:57:49 -0700

    feat: create items for insert in the  table using the factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique->safeEmail,
+            'phone' => function () {
+                $areaCode = '11';
+                $firstPart = '9' . rand(1, 9) . '000';
+                $secondPart = str_pad(rand(1, 9999), 4, '0', STR_PAD_LEFT);
+
+                $phoneNumber = "($areaCode) $firstPart-$secondPart";
+                return $phoneNumber;
+            },
+            'password' => bcrypt($this->faker->password),
+            'money' => 0,
+            'fake_money' => 0
         ];
     }
 
@@ -31,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
